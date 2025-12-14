a5fb90dcc3fbaefa1e7567238ca913ac8dea309b
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-14 10:20:48 +0600

    Done object

---
+++ Part-01/object.js
@@ -0,0 +1,14 @@
+const studentDetails = {
+    name: "Nusrat Zahan Era",
+    age: 18,
+    class: 11,
+    marital_status: "Married",
+    occupation: "Housewife",
+    hobby: "Gardening",
+    favourite_food: "Biriyani",
+    favourite_fruit: "Mango",
+}
+
+
+console.log(studentDetails.name)
+console.log(studentDetails.favourite_food)
