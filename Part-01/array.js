9ed485010b44d6234cd9149b87d61a9407d60fb2
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-14 08:56:45 +0600

    Done array methods

---
+++ Part-01/array.js
@@ -0,0 +1,32 @@
+// ForEach, Map, Filter, Find
+
+
+const arr = [1, 25, 14, 5, 2, 4, 10, 25, 50, 10, 20]
+
+
+// arr.forEach((item) => {
+//     console.log(item + 2)
+// })
+
+
+// const newArr = arr.map((item) => {
+//     return item * 2
+// })
+
+
+// const newArr = arr.filter((item) => {
+//     return item > 20
+// })
+
+
+// const newArr = arr.find((item) => {
+//     return item > 20
+// })
+
+
+const res = arr.indexOf(4454)
+console.log(res)
+
+
+// console.log(arr)
+// console.log(newArr)
