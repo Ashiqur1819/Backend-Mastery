7311f347efbe094757010008a9bc25dc62e65954
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-14 11:45:54 +0600

    Done basic file system

---
+++ Part-02/file-modules/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+// fs.writeFile("era.txt", "Era is a pagli.", (err) => {
+//     if(err){
+//         console.log("I face error", err)
+//     }else{
+//         console.log("Everything is ok")
+//     }
+// })
+
+// fs.appendFile("era.txt", " But she is very honest.", (err) => {
+//     err ? console.log("I face error") : console.log("Done")
+// })
+
+// fs.rename("era.txt", "nusrat.txt", (err) =>{
+// err ? console.log("I face error") : console.log("Done")
+// })
+
+// fs.copyFile("nusrat.txt", "./copy/era.text", (err) => {
+//     err ? console.log("I face error") : console.log("Copy Done")
+// })
+
+// fs.unlink("nusrat.txt", (err) => {
+//     err ? console.log("I face error") : console.log("Remove file Done")
+// })
+
+fs.rm("./copy", {recursive: true}, (err) => {
+    err ? console.log("I face error") : console.log("Remove folder Done")
+})
