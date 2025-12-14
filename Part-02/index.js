d05a2fcfc7ebadcc6a7d865dbf53dfecd6fe2b4b
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-14 11:05:33 +0600

    Append file

--- Part-02/index.js
+++ Part-02/index.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 
-fs.writeFile("era.txt", "Era is a pagli", (err) => {
-    if(err){
-        console.log("I face error", err)
-    }else{
-        console.log("Everything is ok")
-    }
+// fs.writeFile("era.txt", "Era is a pagli.", (err) => {
+//     if(err){
+//         console.log("I face error", err)
+//     }else{
+//         console.log("Everything is ok")
+//     }
+// })
+
+fs.appendFile("era.txt", " But she is very honest.", (err) => {
+    err ? console.log("I face error") : console.log("Done")
 })
