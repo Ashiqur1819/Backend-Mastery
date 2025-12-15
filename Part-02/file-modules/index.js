2adee719b1dd056d0d94f32f6cc074c6be1495f9
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-15 10:52:09 +0600

    update file-modules

--- Part-02/file-modules/index.js
+++ Part-02/file-modules/index.js
@@ -24,6 +24,23 @@ const fs = require('fs');
 //     err ? console.log("I face error") : console.log("Remove file Done")
 // })
 
-fs.rm("./copy", {recursive: true}, (err) => {
-    err ? console.log("I face error") : console.log("Remove folder Done")
+// fs.rm("./copy", {recursive: true}, (err) => {
+//     err ? console.log("I face error") : console.log("Remove folder Done")
+// })
+
+// fs.readFile("hello.txt", "utf-8", (err, data) => {
+//      err ? console.log("I face error") : console.log("READ DATA :", data)
+// })
+
+// fs.mkdir("era.js", (err) => {
+//     err ? console.log("I face error") : console.log("Folder Created")
+// })
+
+// fs.rename("hi.txt", "./demo/hi.txt", (err) => {
+//     err ? console.log("I face error") : console.log("File moved")
+// })
+
+fs.rm("./demo", {recursive: true}, (err) => {
+    err ? console.log("I face error") : console.log("Remove the demo folder")
 })
+
