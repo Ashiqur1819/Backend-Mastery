6985b12e61068e711dc535379660bd9be472b39b
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-16 12:42:43 +0600

    Added dynamic route

--- Part-6/Mini-Project/index.js
+++ Part-6/Mini-Project/index.js
@@ -9,9 +9,23 @@ app.use(express.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 app.use(express.static(path.join(__dirname, "public")))
 
+// app.get("/", (req, res) => {
+//     res.render("index")
+// })
+
 app.get("/", (req, res) => {
-    res.render("index")
+    res.send("Server is running")
 })
+
+
+app.get("/profile/:username", (req, res) => {
+    res.send(req.params.username)
+})
+
+app.get("/profile/:username/:occupation", (req, res) => {
+    res.send(`<h1>Welcome ${req.params.username}, as a ${req.params.occupation}</h1>`)
+})
+
 
 app.listen(port, () => {
     console.log("The server is running on port:", port)
