8a5b7e4e400100766b9482a29887c774d7ffbec2
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-16 12:01:43 +0600

    Added ejs with raw css and js

--- Part-6/Mini-Project/index.js
+++ Part-6/Mini-Project/index.js
@@ -2,14 +2,19 @@ const express = require("express")
 
 const app = express()
 const port = 5000
+const path = require("path")
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")))
 
 app.get("/", (req, res) => {
-    res.send("<h2>The server is running</h2>")
+    res.render("index")
 })
 
 app.listen(port, () => {
     console.log("The server is running on port:", port)
 })
+
+
