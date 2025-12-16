ce95fcbe3658cb9ec46cd9170db0698b5d95197d
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-16 09:47:17 +0600

    Addes basic middleware and erorr handler

--- Part-4/index.js
+++ Part-4/index.js
@@ -2,12 +2,28 @@ const express = require("express")
 
 const app = express()
 
+app.use((req, res, next) => {
+    console.log("This is first middleware")
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log("This is second middleware")
+    next()
+})
+
 app.get("/", (req, res) => {
     res.send("This is the homepage")
 })
 
-app.get("/about", (req, res) => {
-    res.send("This is the about page")
+app.get("/about", (req, res, next) => {
+    // res.send("This is the about page")
+    return next(new Error("Faces some erorr"))
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 })
 
 app.listen(3000)
