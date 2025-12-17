bcbb3088b46ef7b41715f914accf02b00a91193b
ashiqur1929 <mdashiqurrahman1929@gmail.com>
2025-12-17 12:06:27 +0600

    Added edit functionality

--- Part-6/Mini-Project/index.js
+++ Part-6/Mini-Project/index.js
@@ -30,6 +30,16 @@ app.get("/file/:filename", (req, res) => {
     })
 })
 
+app.get("/edit/:filename", (req, res) => {
+    res.render("edit", {filename: req.params.filename})
+})
+
+app.post("/edit", (req, res) => {
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, (err) => {
+        res.redirect("/")
+    })
+})
+
 app.listen(port, () => {
     console.log("The server is running on port:", port)
 })
